bfb101b08b1a5934fd9e4342e3ad9659e98c094b
Laaguidi <aitlaaguidiyassine@gmail.com>
2024-01-06 10:31:23 +0100

    Install all dependencies

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useContext} from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PresentationScreen from "./screens/PresentationScreen";
+//import AllHabits from "./screens/AllHabits";
+//import IconButton from "./components/IconButton";
+
+//import ManageHabit from "./screens/ManageHabit";
+//import HabitsContextProvider, {HabitsContext} from "./store/habits-context";
+
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+                name="Presentation"
+                component={PresentationScreen}
+                //options={{ headerShown: false }}
+            />
+
+          </Stack.Navigator>
+        </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7f8c8d',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '80%'
   },
 });
